7a82bacf89d349602db6dd208a21535ce70e6b9c
OOLMrStone <150834976+OOLMrStone@users.noreply.github.com>
2024-01-15 19:50:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import io
 import sys
 import g4f
 import random
+import spacy
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
@@ -14,6 +15,12 @@ with open("mainWindow.ui", encoding="utf8") as uif:
 with open("endWindow.ui", encoding="utf8") as uif:
     end_template = uif.read()
 
+language_codes = {"English": "en",
+                  "Spanish": "es",
+                  "German": "de",
+                  "Italian": "it",
+                  "Chinese (simplified)": "zh",
+                  "French": "fr"}
 
 def ask_ai(question):
     response = g4f.ChatCompletion.create(
@@ -37,6 +44,12 @@ def translate(word: str, language: str):
     except Exception:
         result = "Проверьте текст на отсутствие опечаток и попробуйте снова."
     return result
+
+
+def to_usual_from(word, language):
+    nlp = spacy.load(f'{language_codes[language]}_core_web_sm')
+    doc = nlp(word)
+    return ' '.join([token.lemma_ for token in doc])
 
 
 class StartWindow(QMainWindow):
@@ -70,7 +83,9 @@ class MainWindow(QMainWindow):
 
         self.words_data = {}
         self.knowledge = {}
+        self.words = None
         self.end_window = None
+        self.start_window = None
 
         self.startBtn.clicked.connect(self.run)
         self.statBtn.clicked.connect(self.view_stat)
@@ -80,9 +95,12 @@ class MainWindow(QMainWindow):
         self.returnButton.clicked.connect(self.make_word_set)
 
     def run(self):
-        self.textBrowser.setText("Приступаем к созданию нового текста...")
+        if self.start_window is None:
+            self.statusLabel.setText("Набор слов отсутствует.")
+            return
+        # TODO: inform user about the text is generating
         self.words_data = {word.split(' - ')[0]: set(word.split(' - ')[1].split(', '))
-                           for word in self.word_window.setP.toPlainText().lower().split('\n')}
+                           for word in self.start_window.setP.toPlainText().lower().split('\n')}
         self.words = [word for word in self.words_data]
         text = ask_ai(prompt(str(self.lang_query.currentText()),
                              str(self.lvl_query.currentText()), ', '.join(self.words)))
@@ -99,6 +117,9 @@ class MainWindow(QMainWindow):
             self.end_window.statView.insertPlainText(f"{'!' * marks} {word} - {percentage}%\n")
 
     def check_translate(self):
+        if self.words is None:
+            self.statusLabel.setText("Набор слов отсутствует.")
+            return
         current_word = self.wordP.text().lower()
         if current_word not in self.words:
             self.statusLabel.setText("Такого слова нет в наборе.")
@@ -106,8 +127,8 @@ class MainWindow(QMainWindow):
 
         self.knowledge[current_word] = self.knowledge.get(current_word, [0, 0])
         self.knowledge[current_word][1] += 1
-        if (self.translateP.text().lower() in self.words_data[current_word] or
-                set(self.translateP.text().lower().split(', ')) == self.words_data[current_word]):
+        if (self.translateP.toPlainText().lower() in self.words_data[current_word] or
+                set(self.translateP.toPlainText().lower().split(', ')) == self.words_data[current_word]):
             self.statusLabel.setText("Верный перевод! Так держать!")
             self.knowledge[current_word][0] += 1
         else:
@@ -118,8 +139,12 @@ class MainWindow(QMainWindow):
         if self.wordP.text().lower() in self.words_data:
             text = ', '.join(self.words_data[self.wordP.text().lower()])
         else:
-            text = translate(self.wordP.text().lower(), "ru")
-        self.translateP.setText(text)
+            base = to_usual_from(self.wordP.text().lower(), self.lang_query.currentText())
+            text = f"Перевод фразы: {translate(self.wordP.text().lower(), 'ru')}.\n\n\n"
+            if base.lower() != self.wordP.text().lower() and self.wordP.text().lower().count(' ') <= 1:
+                text += f"Начальная форма (инфинитив): {base};\n\n"
+                text += f"Перевод: {translate(base, 'ru')}."
+        self.translateP.setPlainText(text)
 
     def end_train(self):
         ex.close()
@@ -129,8 +154,8 @@ class MainWindow(QMainWindow):
             self.view_stat()
 
     def make_word_set(self):
-        self.word_window = StartWindow()
-        self.word_window.show()
+        self.start_window = StartWindow()
+        self.start_window.show()
 
 
 class EndWindow(QMainWindow):
